233c30ce3ff7e80b2c28a1c404493ddc5204e968
Jimmy-36 <89508072+Jimmy-36@users.noreply.github.com>
2022-01-18 16:56:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
-const backpack = {
-  name: "Everyday Backpack",
-  volume: 30,
-  color: "grey",
-  pocketNum: 15,
-  strapLength: {
-    left: 26,
-    right: 26,
-  },
-  lidOpen: false,
-  toggleLid: function (lidStatus) {
-    this.lidOpen = lidStatus;
-  },
-  newStrapLength: function (lengthLeft, lengthRight) {
-    this.strapLength.left = lengthLeft;
-    this.strapLength.right = lengthRight;
-  },
+const cellphone = {
+    name: "Everyday phone",
+    battery: "small battery",
+    batteryLife: 12,
+    model: "iphone 5s",
+    dimensions: {
+        length: 2.5,
+        width: 5.5,
+    },
+    newdimensions: function (newLength, newWidth) {
+        this.dimensions.length = newLength;
+        this.dimensions.width = newWidth;
+    },
 };
-
-console.log("The backpack object:", backpack);
-console.log("The pocketNum value:", backpack.pocketNum);
-
-console.log('left before:', backpack.strapLength.left);
-
-backpack.newStrapLength(10,15);
-
-console.log('left after:', backpack.strapLength.left);
